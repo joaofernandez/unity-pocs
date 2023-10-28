db2216bf31dce095cd60c68719b0068ec5bdf60a
João Fernandez <joaorbfernandez@gmail.com>
2023-10-28 11:29:47 -0300

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    private readonly int SPEED = 5;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * SPEED * Time.fixedDeltaTime);
+    }
+}
